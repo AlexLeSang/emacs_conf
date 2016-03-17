3798cd7c8eca202c5eaa141ebee9e4caf14f6df7
Oleksandr Halushko <alexlesang@gmail.com>
2016-03-17 16:35:20 +0100

    Add missing directories

---
+++ emacs.d/elpa/github-theme-0.0.3/github-theme-autoloads.el
@@ -0,0 +1,16 @@
+;;; github-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil nil ("github-theme.el") (22130 62085 818110
+;;;;;;  773000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; github-theme-autoloads.el ends here
